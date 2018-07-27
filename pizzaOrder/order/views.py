b441a549334566b844b75da98bfa1236ad17cefc
Cornel Remer <cornel.remer@gmail.com>
2018-07-27 15:45:32 +0200

    stdt functions with rest-frameworks router

--- pizzaOrder/order/views.py
+++ pizzaOrder/order/views.py
@@ -1,20 +1,8 @@
-from django.shortcuts import get_object_or_404  # return 404 if object dosen' exist
-from rest_framework.views import APIView        # return API data
-from rest_framework.response import Response    # send back specific response status (e.g. 200)
-from rest_framework import status
+from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Order
-from . serializeJSON import OrderSerializer
+from .serializers import OrderSerializer
 
-# set as class based view
-class OrderList(APIView):
-
-    # list all orders
-    def get(self, request):
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-
-        return Response(serializer.data)
-
-    # create new order
-    def post(self):
-        pass
+class OrderView(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
